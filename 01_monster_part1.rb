3e187f3beeaa0864d6a6c8a1cb76e4010e5e4e71
chunchaolee <chunchaolee@gmail.com>
2017-11-29 10:50:50 +0800

    clone oop from AC

--- 01_monster_part1.rb
+++ 01_monster_part1.rb
@@ -4,7 +4,7 @@ class Monster
     @name = name                              # 設定一個名為 name（名稱）的 attribute
     @hp = hp                                  # 設定一個名為 hp（生命值）的 attribute
     @ap = ap                                  # 設定一個名為 ap（攻擊值）的 attribute
-    @alive = true                             # 怪獸剛被創造，所以預設為 true，表示怪獸創造時一定是活著的
+    @alive = true                             # 怪獸剛被創造，所以預設為 true，表示怪獸創造時一定是活著
     
     # 印出被創造的怪獸的 attributes
     puts "遇到怪獸 #{@name} 了!"
